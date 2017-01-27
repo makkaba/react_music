6e7902087bcd6bfef57af29db3c3146561370552
Jeff Chung <drifterz303@gmail.com>
2017-01-27 23:45:20 +0900

    장르를 추가한다

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -4,15 +4,31 @@ import './App.css';
 export default class Profile extends Component {
   render() {
     console.log('this.props', this.props);
-    let artist = {name: '', followers: {total: ''}};
+    let artist = {name: '', followers: {total: ''}, images: [{url: ''}], genres: []};
+    artist = this.props.artist !== null ? this.props.artist : artist;
     if(this.props.artist !== null) {
       artist = this.props.artist;
     }
     
     return (
-      <div>
+      <div className="profile">
+        <img
+          alt="Profile"
+          className="profile-img"
+          src={artist.images[0].url}
+        />
         <div>{artist.name}</div>
         <div>{artist.followers.total}</div>
+        <div>
+          {
+            artist.genres.map((genre, index )=> {
+              genre = genre !== artist.genres[artist.genres.length-1] ? ` ${genre},` : ` & ${genre}`;
+              return (
+                <span key={index}>{genre}</span>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
