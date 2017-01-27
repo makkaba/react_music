805851f5fac5690c283d5bd3d21da1b7b8d52977
Jeff Chung <drifterz303@gmail.com>
2017-01-27 23:52:36 +0900

    검색 결과에 스타일을 추가한다

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -17,17 +17,19 @@ export default class Profile extends Component {
           className="profile-img"
           src={artist.images[0].url}
         />
-        <div>{artist.name}</div>
-        <div>{artist.followers.total}</div>
-        <div>
-          {
-            artist.genres.map((genre, index )=> {
-              genre = genre !== artist.genres[artist.genres.length-1] ? ` ${genre},` : ` & ${genre}`;
-              return (
-                <span key={index}>{genre}</span>
-              )
-            })
-          }
+        <div className="profile-info">
+          <div className="profile-name">{artist.name}</div>
+          <div className="profile-followers">{artist.followers.total}</div>
+          <div className="profile-genres">
+            {
+              artist.genres.map((genre, index )=> {
+                genre = genre !== artist.genres[artist.genres.length-1] ? ` ${genre},` : ` & ${genre}`;
+                return (
+                  <span key={index}>{genre}</span>
+                )
+              })
+            }
+          </div>
         </div>
       </div>
     )
